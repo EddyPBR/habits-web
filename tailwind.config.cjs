9e222c0ccb345ef8d81d9291f44476adfde86313
EddyPBR <edvaldojunior1310@gmail.com>
2023-01-18 23:43:20 -0300

    :wrench: config tailwind, postcss and add new class in index.html

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./src/**/*.tsx", 
-    "./index.html"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#09090a",
+      },
+    },
+    gridTemplateRows: {
+      7: "repeat(7, minmax(0, 1fr)",
+    },
   },
   plugins: [],
 };
